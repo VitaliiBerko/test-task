1a34f0ad001cbb71ca086acb7dd0b812771125b0
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-05-17 17:00:46 +0300

    Counter followers

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const startFollowers = `${100500+count}`.split("")
+  startFollowers.splice(-3, 0, ",")
+
+  const numberFollowers = (startFollowers.join(""))
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      {<h2>{numberFollowers} FOLLOWERS</h2>}
+      <button onClick={() => setCount((count) => count + 1)}>
+          FOLLOW
+        </button>
+    </>
+  )
+}
+
+export default App
+
+
+{/* <div>
+        <Link></Link>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -27,9 +47,4 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      </p> */}
