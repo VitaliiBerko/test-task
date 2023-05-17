b13c2f1465c0eedff0e40ea1247ea3a4697f2f4d
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-05-17 13:11:17 +0300

    Deploy test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-'https://github.com/VitaliiBerko/test-task'
 export default defineConfig({
   plugins: [react()],
+  base: "/test-task"
 })
