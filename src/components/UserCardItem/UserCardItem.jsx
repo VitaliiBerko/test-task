c7384ce76737258e212d8e60e455cd045ae1da50
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-05-19 16:34:44 +0300

    Added styles and fixed counter

--- src/components/UserCardItem/UserCardItem.jsx
+++ src/components/UserCardItem/UserCardItem.jsx
@@ -1,31 +1,49 @@
 import { useState } from "react";
-import "./UserCardItem.styled.css";
+import "./UserCardItemStyles.scss";
+import logo from "../../images/Logo.svg";
+import picture from "../../images/picture@1.png";
+import avatar from "../../images/Hansel.png";
+import ellips from "../../images/Ellipse1.png";
 
 const UserCardItem = () => {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("FOLLOW");
+  const [isActive, setIsActive] = useState(false);
 
   const startFollowers = `${100500 + count}`.split("");
   startFollowers.splice(-3, 0, ",");
   const numberFollowers = startFollowers.join("");
-  const button= document.querySelector("#follow-button")
-  console.log(button);
 
   const handleOnClick = () => {
-    setCount(count + 1);
-    setText("FOLLOWING");
-    button.classList.toggle('active')    
+    setIsActive(!isActive);
+
+    if (!isActive) {
+      setCount(count + 1);
+      setText("FOLLOWING");
+      document.querySelector("#follow-button").classList.add("active");
+    } else {
+      setCount(count - 1);
+      setText("FOLLOW");
+      document.querySelector("#follow-button").classList.remove("active");
+    }
   };
+
   return (
     <li className="user-card">
-        <img src="" alt="" />
-      <ul>
-        <li>777 TWEETS</li>
-        <li>
-          {numberFollowers} FOLLOWERS          
-        </li>
+      <img src={logo} alt="Logo" className="logoGO" />
+      <img src={picture} alt="Ideas" width="308px" height="168px" />
+      <div className="thumb">
+        <img src={ellips} alt="Ellips" />
+        <img src={avatar} alt="User avatar" className="avatar" />
+      </div>
 
-        <button onClick={handleOnClick} id="follow-button">{text}</button>
+      <ul>
+        <li className="text">777 TWEETS</li>
+        <li className="text">{numberFollowers} FOLLOWERS</li>
+
+        <button onClick={handleOnClick} id="follow-button" className="button">
+          {text}
+        </button>
       </ul>
     </li>
   );
