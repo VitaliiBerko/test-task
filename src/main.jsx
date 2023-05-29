50f46128cb25c826310e523fc34dcb5f3966cc8b
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-05-29 16:17:53 +0300

    Testing 404html

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter
-    //  basename='/test-task'
+    <BrowserRouter
+     basename='/test-task'
      >
       <Provider store={store}>
         <PersistGate loading ={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
